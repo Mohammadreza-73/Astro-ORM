8c3016ba6273e67b66ad818127294c4c7b573640
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2021-11-23 16:19:01 +0330

    Initialize git:
    Add layouts, database configs to getting start prject.

---
+++ src/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'pdo' => [
+        'dirver' => 'mysql',
+        'host' => 'localhost',
+        'db_name' => 'astro_orm',
+        'db_user' => 'root',
+        'db_password' => '',
+    ],
+    'pdo_testing' => [
+        'dirver' => 'mysql',
+        'host' => 'localhost',
+        'db_name' => 'orm_testing',
+        'db_user' => 'root',
+        'db_password' => '',
+    ]
+];
