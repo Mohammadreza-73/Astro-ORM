58acfe30a99eeddfdc09482719538a0b1492eadd
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2021-11-30 16:32:40 +0330

    Add create test:
    Insert data in database with http post.

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
+use App\Database\PDOQueryBuilder;
+
 require_once "./vendor/autoload.php";
 
+function json_response(?array $data = null, int $statusCode = 200)
+{
+    header_remove();
+    header('Content-Type: application/json');
+    http_response_code($statusCode);
+    echo json_encode($data);
+    exit();
+}
+
+function request()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    PDOQueryBuilder::table('users')->create(request());
+
+    json_response(null, 200);
+}
--- tests/Functional/CrudTest.php
+++ tests/Functional/CrudTest.php
@@ -19,7 +19,26 @@ class CrudTest extends TestCase
         parent::setUp();
     }
 
+    public function testItCanCreateDataWithApi(): void
+    {
+        $data = [
+            'json' => [
+                'name' => 'Api',
+                'email' => 'api@gmail.com',
+                'skill' => 'restfull'
+            ]
+        ];
 
+        $response = $this->httpClient->post('index.php', $data);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $result = PDOQueryBuilder::table('users')
+            ->where('name', 'Api')
+            ->where('skill', 'restfull')
+            ->first();
+
+        $this->assertNotNull($result);
+    }
 
     public function tearDown(): void
     {
